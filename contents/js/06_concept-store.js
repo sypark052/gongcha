a0e9cc2e58e7139cffcf92de5bc53dc242c76448
sypark052 <syzh00@naver.com>
2024-12-31 17:23:56 +0900

    추가

--- contents/js/06_concept-store.js
+++ contents/js/06_concept-store.js
@@ -17,19 +17,59 @@ $(document).ready(function () {
 //fadeSlide 함수 만들기
 function fadeSlide() {
     //1번 슬라이드
-    var firstSlide = $('.img-box .slide').first();
+
+    //var firstSlide = $('.img-box .slide').first();
+
+    //var firstSlide = $('.img-box .slide:first-child');
+
+    var b1 =$('.b1 .img-box .slide').first();
+    var b2 =$('.b2 .img-box .slide').first();
+    var b3 =$('.b3 .img-box .slide').first();
+
+
 
     //2번 슬라이드
-    var nextSlide = firstSlide.next();
+    //var nextSlide = firstSlide.next();
 
-    nextSlide.hide().addClass('active').fadeIn(800, function () {
+    var nextSlide1 = b1.next();
+    var nextSlide2 = b2.next();
+    var nextSlide3 = b3.next();
+
+    //세개 넣어보세요
+
+
+
+    nextSlide1.hide().addClass('active').fadeIn(800, function () {
        
-        //1번 슬라이드를 맨뒤로 이동!
 
-        $('.img-box').append(firstSlide);
+        //1번 슬라이드를 맨뒤로 이동!
+        //$('.slide').append(firstSlide);
+        //$('.img-box').append(firstSlide);
+
+        $('.b1 .img-box').append(b1);
 
         //첫 번째 슬라이드의 z-index 제거!
-        firstSlide.removeClass('active');
+        //firstSlide.removeClass('active');
+
+        b1.removeClass('active');
     });
+
+
+    nextSlide2.hide().addClass('active').fadeIn(800,function(){
+
+        $('.b2 .img-box').append(b2);
+
+        b2.removeClass('active');
+    });
+
+
+    nextSlide3.hide().addClass('active').fadeIn(800,function(){
+
+        $('.b3 .img-box').append(b3);
+
+        b3.removeClass('active');
+    });
+
+
 
 }
